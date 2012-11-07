2ca2947631f41c8b1109bbf482c3105ffbb2a234
Kaspar M. Rohrer <kaspar.rohrer@gmail.com>
2012-11-07 18:55:20 +0100

    Root check in install script removed.

--- install.bash
+++ install.bash
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [ `whoami` == 'root' ]
-then
-ln ~/.profile.d/profile ~/.profile
-ln ~/.profile.d/bashrc ~/.bashrc
-cp ~/.profile.d/bashrc /etc/bashrc
-else
-    echo "You must be root to install."
-fi
+ln -i ~/.profile.d/profile ~/.profile
+ln -i ~/.profile.d/bashrc ~/.bashrc
+cp -i ~/.profile.d/bashrc /etc/bashrc
